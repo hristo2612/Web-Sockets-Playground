a2f9721578bc5d7ba48d72db63ba134615ad1e03
hristo2612 <hristoapps@gmail.com>
2021-10-11 00:51:44 +0300

    Refactor Websockets

--- api/websocket.js
+++ api/websocket.js
@@ -4,21 +4,24 @@ const webSocketCallback = (expressWs) => async (ws, req) => {
   ws.on('message', (message) => {
     insertMessage(JSON.parse(message).data);
     const allClients = expressWs.getWss().clients;
-    allClients.forEach((client) => {
-      if (client.readyState === client.OPEN) {
-        const data = JSON.parse(message).data;
-        const msg = JSON.stringify({
-          text: data.text,
-          userName: data.userName
-        });
-        client.send(msg);
-      }
-    });
+    sendMessageToAllClients(allClients, message);
   });
 
   const messages = await getMessages();
   ws.send(JSON.stringify(messages));
+}
 
+const sendMessageToAllClients = (clients, message) => {
+  clients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      const data = JSON.parse(message).data;
+      const msg = JSON.stringify({
+        text: data.text,
+        userName: data.userName
+      });
+      client.send(msg);
+    }
+  });
 }
 
 exports.webSocketCallback = webSocketCallback;
